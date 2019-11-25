1c1090c396efbe87b1204a43a33c8fb644149c7d
yemiancheng <ymc.github@gmail.com>
2019-11-25 11:09:31 +0800

    feat(core): deleting path delimeter

--- build.sh
+++ build.sh
@@ -14,21 +14,21 @@ source ${basepath}/lang.sh
 ##########
 now_step=step_00
 echo "# $now_step -> ${lang_steps[$now_step]}"
-docker build --tag ${hub_address}/${hub_ns}/${hub_tag} ${my_dockerfile_path}
-# docker-machine ssh $my_vm_name "docker build --tag ${hub_address}/${hub_ns}/${hub_tag} ${my_dockerfile_path}"
+docker build --tag ${hub_address}${hub_ns}${hub_tag} ${my_dockerfile_path}
+# docker-machine ssh $my_vm_name "docker build --tag ${hub_address}${hub_ns}${hub_tag} ${my_dockerfile_path}"
 
 now_step=step_01
 echo "# $now_step -> ${lang_steps[$now_step]}"
 #docker images
 #docker-machine ssh $my_vm_name "docker images"
-image_id=$(docker images --filter=reference="${hub_address}/${hub_ns}/${hub_tag}" --format "{{.ID}}")
+image_id=$(docker images --filter=reference="${hub_address}${hub_ns}${hub_tag}" --format "{{.ID}}")
 echo "$my image id is ${image_id}"
 
 now_step=step_02
 echo "# $now_step -> ${lang_steps[$now_step]}"
 #docker run -d ${image_id} docker ps
-docker run --rm ${hub_address}/${hub_ns}/${hub_tag} ls -al /
-#docker-machine ssh $my_vm_name "docker run --rm ${hub_address}/${hub_ns}/${hub_tag} ls -al /"
+docker run --rm ${hub_address}${hub_ns}${hub_tag} ls -al /
+#docker-machine ssh $my_vm_name "docker run --rm ${hub_address}${hub_ns}${hub_tag} ls -al /"
 
 
 now_step=step_03
@@ -38,12 +38,11 @@ docker login -u ${user_u} -p ${user_p} ${hub_address}
 
 now_step=step_04
 echo "# $now_step -> ${lang_steps[$now_step]}"
-docker tag  ${image_id} ${hub_address}/${hub_ns}/${hub_tag}
-#docker-machine ssh $my_vm_name "docker tag ${image_id} ${hub_address}/${hub_ns}/${hub_tag}"
+docker tag  ${image_id} ${hub_address}${hub_ns}${hub_tag}
+#docker-machine ssh $my_vm_name "docker tag ${image_id} ${hub_address}${hub_ns}${hub_tag}"
 
 now_step=step_05
 echo "# $now_step -> ${lang_steps[$now_step]}"
-docker push ${hub_address}/${hub_ns}/${hub_tag}
-#docker-machine ssh $my_vm_name "docker push ${hub_address}/${hub_ns}/${hub_tag}"
+docker push ${hub_address}${hub_ns}${hub_tag}
+#docker-machine ssh $my_vm_name "docker push ${hub_address}${hub_ns}${hub_tag}"
 
-#build-sh
--- conf.sh
+++ conf.sh
@@ -5,13 +5,13 @@
 ######
 image_id=
 # 镜像仓库用户账户
-user_u=hualei03042013@163.com
+user_u=yemiancheng
 # 镜像仓库用户密码
-user_p=
+user_p=3fgUWJhgszVYNb4
 # 镜像仓库地址
-hub_address=hub.c.163.com
+hub_address= #hub.c.163.com
 # 镜像仓库空间
-hub_ns=yemiancheng
+hub_ns=yemiancheng/
 # 镜像标签
 hub_tag=alpine-3-8nodejs-8-11-4
 
@@ -21,7 +21,7 @@ hub_tag=alpine-3-8nodejs-8-11-4
 # 虚拟机器名字
 my_vm_name=default
 # 镜像配置文件位置
-my_dockerfile_path=/docker/bin/mirro-quick-build
+my_dockerfile_path=.
 
 ######
 ### 提示
@@ -29,4 +29,3 @@ my_dockerfile_path=/docker/bin/mirro-quick-build
 # 当前步骤
 now_step=
 # 下一步骤
-go_to_next_step=
